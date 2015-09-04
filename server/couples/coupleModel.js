ba0a3aee461362911ec9ae04a7593f28b5b957da
Jackson Sierra <jks718@gmail.com>
2015-09-04 15:35:46 -0700

    [refactor] (db): refactor parameterized queries

---
+++ server/couples/coupleModel.js
@@ -0,0 +1,19 @@
+var dbConnection = require('../db/index.js');
+
+var queryDb = function(queryString, params, callback) {
+  dbConnection.query(queryString, params, function(err, results) {
+    if(err) {
+      console.error('Database Error: ' + err);
+    }
+
+    callback(results);
+  });
+};
+
+module.exports = {
+  
+  postCouple: function(params, callback) {
+    var queryString = 'INSERT INTO couples (person_1_last_name, person_1_first_name, person_2_last_name, person_2_first_name, token, email, phone, photo_filepath) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    queryDb(queryString, params, callback);
+  }
+};
