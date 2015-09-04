9e08f93e2cae55c311c80342c98c0258c7f5ae0b
codeNameAtlas <anavarrete255@gmail.com>
2015-09-04 11:47:43 -0700

    [refactor] (server): add couple router connecting couple controller

---
+++ server/couples/coupleRoute.js
@@ -0,0 +1,9 @@
+var coupleCtrl = require('./coupleCtrl.js');
+
+
+module.exports = function (app) {
+  // app === userRouter injected from middlware.js
+
+  app.post('/signin', coupleCtrl.signin);
+  app.post('/signup', coupleCtrl.signup);
+};
