d6852b7916b7aafce7d5cb2860b97520d09d5a3a
codeNameAtlas <anavarrete255@gmail.com>
2015-09-03 17:18:56 -0700

    [server] added static server and dependecy in package.json

---
+++ server/server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/client'));
+app.listen(process.env.PORT || 3000);
+
+var port = process.env.PORT || 3000;
+
+console.log("listening on " + port);
