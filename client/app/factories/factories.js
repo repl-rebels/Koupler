1eb24fc3602e86ddee7a5202797bb9b6564782d4
Shreeya <shreeya.goel@gmail.com>
2015-09-07 14:02:25 -0700

    [refactor] (client): minor changes to token handoff in getCouples

--- client/app/factories/factories.js
+++ client/app/factories/factories.js
@@ -16,8 +16,8 @@ angular.module('koupler.factories', [])
     var token = $window.localStorage.getItem('JWT');
     $http.post('/activities/match', {activity:activity, token: token});
       .then(function(response) {
-        console.log('couple sent back by database is ' + response);
-        $window.localStorage.setItem('coupleChosen', response); //expected to save chosenCouple in state of the app
+        console.log('couple sent back by database is ' + response.data.couple);
+        $window.localStorage.setItem('coupleChosen', response.data.couple); //expected to save chosenCouple in state of the app
         $location.path('/match');
       }, function(response) {
         console.log('sorry, there was an error ', response.statusText);
