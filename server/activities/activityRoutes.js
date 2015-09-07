53fb2818e4133d969deda97315298fff5670c6d1
codeNameAtlas <anavarrete255@gmail.com>
2015-09-07 14:27:54 -0700

    [refactor] (server) : activityCtrl and Route in progress

--- server/activities/activityRoutes.js
+++ server/activities/activityRoutes.js
@@ -2,5 +2,7 @@ var matchController = require('./activityCtrl');
 
 module.exports = function(app) {
   app.get('/', activityCtrl.getActivities);
-  app.post('/match', activityCtrl.matchCouple);
+  app.route('/match')
+    .post(activityCtrl.matchCouple);
+    .get(activityCtrl.getMatch);
 }
